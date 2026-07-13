835cfb784d265ff8adcbf02afc101f915ccafbd6
wooski515 <132147804+wooski515@users.noreply.github.com>
2026-07-13 13:06:11 +0300

    Add files via upload

---
+++ images.js
@@ -0,0 +1,25 @@
+const IMAGES = {
+    tsukor: [
+        "1.png",
+        "2.png",
+        "3.png",
+        "4.png",
+        "5.png",
+        "6.png",
+        "7.png",
+        "8.png",
+        "9.png",
+        "10.png",
+        "11.png",
+        "12.png",
+        "13.png",
+        "14.png",
+        "15.png",
+    ],
+    sil: [
+        "1.png",
+    ],
+    perets: [
+        "1.png",
+    ],
+};
